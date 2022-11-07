3be8610293a9f32ff1b45316d545e70cd52bfac3
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-11-08 00:37:23 +0600

    aded food folder and create hero section on home page

--- src/Pages/Home/Foods/Food/Food.js
+++ src/Pages/Home/Foods/Food/Food.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Food = () => {
+
+    const [foods, setFoods] = useState([]);
+    useEffect(() =>{
+        fetch('')
+        .then(res => res.json())
+        .then(data => setFoods(data))
+    },[])
     return (
         <div>
-            
+            <div className='text-center'>
+            <h2 className='text-5xl font-semibold'>Our Organic Food</h2>
+            </div>
         </div>
     );
 };
